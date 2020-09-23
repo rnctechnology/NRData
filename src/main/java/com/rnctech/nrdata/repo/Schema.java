bee81ea590f1ad98989c009ced560c4d412d5aa5
Zilin_Chen <zilinchen@yahoo.com>
2020-09-23 10:29:20 -0700

    'add_models'

--- src/main/java/com/rnctech/nrdata/repo/Schema.java
+++ src/main/java/com/rnctech/nrdata/repo/Schema.java
@@ -1,9 +1,12 @@
 package com.rnctech.nrdata.repo;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -28,5 +31,9 @@ public class Schema extends JobBase {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "version", unique = true, nullable = false)
 	long version = 0l;
+	
+	@ManyToOne(cascade = { CascadeType.MERGE, CascadeType.REFRESH })
+	@JoinColumn(name = "userid", referencedColumnName = "id")
+	private User user;
 	
 }
